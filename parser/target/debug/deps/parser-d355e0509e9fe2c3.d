45629b79bbffbd8e19ffb63e58e2673be0254b7a
vasiliy1305 <basil.anenkov@yandex.ru>
2026-03-31 11:00:09 +0300

    init

---
+++ parser/target/debug/deps/parser-d355e0509e9fe2c3.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Desktop\RUST\rust-1\parser\target\debug\deps\parser-d355e0509e9fe2c3.d: src\lib.rs
+
+C:\Users\Admin\Desktop\RUST\rust-1\parser\target\debug\deps\libparser-d355e0509e9fe2c3.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ parser/target/debug/deps/parser-dbef8f72841f887b.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Desktop\RUST\rust-1\parser\target\debug\deps\parser-dbef8f72841f887b.d: src\lib.rs
+
+C:\Users\Admin\Desktop\RUST\rust-1\parser\target\debug\deps\libparser-dbef8f72841f887b.rmeta: src\lib.rs
+
+src\lib.rs:
